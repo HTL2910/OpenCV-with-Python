6b2ae09ced057e6efa26b1347f924b77926d6de6
HTL2910 <hathanhlong2910@gmail.com>
2024-11-10 22:58:27 +0700

    transform image

--- OpenCv.py
+++ OpenCv.py
@@ -1,6 +1,6 @@
 import OpenCv
 import cv2
-
+import numpy as np
 color=(200,100,200)
 
 def show_image(image):
@@ -25,6 +25,24 @@ def add_text(image):
     text="Hi Python"
     cv2.putText(image,text,(500,500),font,font_scale,color,1)
     cv2.imshow('text image',image)
+
+def rotate_image(image):
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+
+    rotation_matrix = cv2.getRotationMatrix2D(center, 45, 1.0)  # Rotate by 45 degrees
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h))
+    cv2.imshow('rotate',rotated_image)
+
+def scaling_image(image):
+    scaled_image = cv2.resize(image, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
+    cv2.imshow('scale image',scaled_image)
+
+def translate_image(image):
+    (h, w) = image.shape[:2]
+    translation_matrix = np.float32([[1, 0, 50], [0, 1, 50]])  # Translate by 50 pixels in x and y directions
+    translated_image = cv2.warpAffine(image, translation_matrix, (w, h))
+    cv2.imshow('translate image',translated_image)
 if __name__=="__main__":
     image = cv2.imread('chessboard.png')
     # show_image(image)
@@ -32,7 +50,10 @@ if __name__=="__main__":
     # resize_image(image)
     # draw(image)
     # add_text(image)
+    # rotate_image(image)
+    # scaling_image(image)
 
+    # translate_image(image)
     
     cv2.waitKey(0)  # Wait until any key is pressed
     cv2.destroyAllWindows()
